d29b5ad7b24d19a9648a4aae8443967ac1d0d3f2
Radical edd <radical3dd@gmai.com>
2023-12-11 18:31:37 +0100

    impl rsrtl

--- mesonbuild/modules/rsrtl.py
+++ mesonbuild/modules/rsrtl.py
@@ -28,9 +28,9 @@ if T.TYPE_CHECKING:
     from ..interpreterbase.baseobjects import TYPE_kwargs, TYPE_var
     from ..programs import ExternalProgram
 
-class CxxrtlModule(NewExtensionModule):
+class RsrtlModule(NewExtensionModule):
 
-    INFO = ModuleInfo('cxxrtl')
+    INFO = ModuleInfo('rsrtl')
 
     def __init__(self, interp: Interpreter) -> None:
         super().__init__()
@@ -38,18 +38,15 @@ class CxxrtlModule(NewExtensionModule):
         self.include_dir = None
         self.methods.update({
             'generate': self.generate,
-            'sim': self.sim,
-            'get_sources': self.get_sources,
             'get_includes': self.get_includes,
         })
 
 
     def detect_tools(self, state: ModuleState) -> None:
         self.tools['yosys'] = state.find_program('yosys')
-        self.tools['cxxrtl-driver'] = state.find_program('cxxrtl-driver')
 
-    @typed_pos_args('cxxrtl.generate', str, (str, File, build.CustomTarget, build.CustomTargetIndex, build.GeneratedList))
-    @typed_kwargs('cxxrtl.generate', KwargInfo('script', str, default='hierarchy -top main; write_cxxrtl -O0 -print-output std::cerr @OUTPUT@'), KwargInfo('tcl', bool, default=False))
+    @typed_pos_args('rsrtl.generate', str, (str, File, build.CustomTarget, build.CustomTargetIndex, build.GeneratedList))
+    @typed_kwargs('rsrtl.generate', KwargInfo('script', str, default='hierarchy -top main; write_cxxrtl -O0 -print-output std::cerr @OUTPUT@'), KwargInfo('tcl', bool, default=False))
     def generate(self, state: ModuleState, args: T.Tuple[str, T.List[T.Union[FileOrString, build.GeneratedTypes]]], kwargs: TYPE_kwargs) -> None:
         if not self.tools:
             self.detect_tools(state)
@@ -81,45 +78,6 @@ class CxxrtlModule(NewExtensionModule):
 
         return ModuleReturnValue(cc_target, [cc_target])
 
-    @typed_pos_args('cxxrtl.sim', str, (str, File, build.BuildTarget))
-    @typed_kwargs('cxxrtl.sim', KwargInfo('data', (str, File)), KwargInfo('interface', (str, File, build.CustomTarget, build.CustomTargetIndex, build.GeneratedList)), KwargInfo('vcd', bool, default=False))
-    def sim(self, state: ModuleState, args: T.Tuple[str, T.Union[FileOrString, build.BuildTargetTypes]], kwargs: TYPE_kwargs) -> None:
-        if not self.tools:
-            self.detect_tools(state)
-
-        proj_name, design = args
-        data = kwargs['data']
-        interface = kwargs['interface']
-        vcd = kwargs['vcd']
-
-        if isinstance(data, str):
-            data = File.from_source_file(state.environment.source_dir, state.subdir, data)
-
-        if isinstance(interface, str):
-            interface= File.from_source_file(state.environment.source_dir, state.subdir, interface)
-
-        if isinstance(interface, build.GeneratedList):
-            s = interface.get_outputs()[0]
-            rel_src = state.backend.get_target_generated_dir(self, interface, s)
-            interface = File.from_built_relative(rel_src)
-
-        cmd_array = [self.tools['cxxrtl-driver'], '--design', design, '--data', data, '--interface', interface]
-        if vcd:
-            cmd_array.push('--vcd')
-        
-        out_target= build.CustomTarget(
-            f'{proj_name}_sim',
-            state.subdir,
-            state.subproject,
-            state.environment,
-            cmd_array,
-            [],
-            [f'{proj_name}.sim'],
-            capture=True
-        )
-
-        return ModuleReturnValue(out_target,  [out_target])
-
     def get_include_dir(self, state: ModuleState) -> str:
         if not self.include_dir:
             if not self.tools:
@@ -135,13 +93,8 @@ class CxxrtlModule(NewExtensionModule):
 
     @noKwargs
     @noPosargs
-    def get_sources(self, state: ModuleState, args: T.List[TYPE_var], kwargs: TYPE_kwargs) -> str:
-        return "{}/backends/cxxrtl/cxxrtl_capi.cc".format(self.get_include_dir(state))
-
-    @noKwargs
-    @noPosargs
     def get_includes(self, state: ModuleState, args: T.List[TYPE_var], kwargs: TYPE_kwargs) -> str:
         return self.get_include_dir(state)
 
-def initialize(interp: Interpreter) -> CxxrtlModule:
-    return CxxrtlModule(interp)
+def initialize(interp: Interpreter) -> RsrtlModule:
+    return RsrtlModule(interp)
